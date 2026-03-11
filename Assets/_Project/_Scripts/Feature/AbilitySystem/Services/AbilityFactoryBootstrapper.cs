63ec7720bc12b1a0615974b455a43c9d0f927923
sabanmeteturkay <sabanmete57@gmail.com>
2026-03-11 15:57:12 +0300

    feat(ability system): ability factory added

---
+++ Assets/_Project/_Scripts/Feature/AbilitySystem/Services/AbilityFactoryBootstrapper.cs
@@ -0,0 +1,24 @@
+using CaseStudy.Feature.AbilitySystem.Abilities;
+using CaseStudy.Feature.AbilitySystem.Contracts;
+using CaseStudy.Feature.AbilitySystem.Data;
+using VContainer.Unity;
+
+namespace CaseStudy.Feature.AbilitySystem.Services
+{
+    public sealed class AbilityFactoryBootstrapper : IStartable
+    {
+        private readonly IAbilityFactory _abilityFactory;
+
+        public AbilityFactoryBootstrapper(IAbilityFactory abilityFactory)
+        {
+            _abilityFactory = abilityFactory;
+        }
+
+        public void Start()
+        {
+            _abilityFactory.Register<ProjectileAbilityDataSO>(_ => new ProjectileAbility());
+            _abilityFactory.Register<AoeAbilityDataSO>(_ => new AoeAbility());
+            _abilityFactory.Register<DashAbilityDataSO>(_ => new DashAbility());
+        }
+    }
+}
